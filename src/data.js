bb95181b00c299968f115aef0415517437d38b98
catalin.moraru <catalinmoraru82@gmail.com>
2023-03-22 21:49:14 +0200

    update components

---
+++ src/data.js
@@ -0,0 +1,24 @@
+import img1 from './images/image-01.jpeg';
+import img2 from './images/image-02.jpeg';
+import img3 from './images/image-03.jpg';
+
+export const books = [
+    {
+      author: 'Jess Lourey',
+      title: 'The Quarry Girls: A Thriller',
+      img: img1,
+      id: 1,
+    },
+    {
+      author: 'Freida McFadden',
+      title: 'Never Lie',
+      img: img2,
+      id: 2,
+    },
+    {
+      author: 'James Patterson',
+      title: 'Fear No Evil (Alex Cross Book 29)',
+      img: img3,
+      id: 3,
+    },
+]
--- src/index.js
+++ src/index.js
@@ -1,46 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { books } from './data';
+import  Book from './Book';
 import './index.css';
-
-const books = [
-  {
-    author: 'Jess Lourey',
-    title: 'The Quarry Girls: A Thriller',
-    img: '../images/image-01.jpeg',
-    id: 1,
-  },
-  {
-    author: 'Freida McFadden',
-    title: 'Never Lie',
-    img: './images/image-02.jpeg',
-    id: 2,
-  }
-]
 
 const Booklist = () => {
   return (
-    <section className="booklist">
-      {
-        books.map((book) => {
-          return <Book {...book} key={book.id}/>
-        })
-      }
-    </section>
-  )
-}
-
-const Book = ({author, title, img}) => {
-  return (
-    <article className="book">
-        <img src={img} alt={title} />
-        <h2>{title}</h2> 
-        <h4>{author.toUpperCase()}</h4>
-    </article>
+    <>
+      <h1>Best selling books</h1>
+      <section className="booklist">
+        {
+          books.map((book, index) => {
+            return <Book {...book} key={book.id} number={index}/>
+          })
+        }
+      </section>
+    </>
   )
 }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render (
   <Booklist />
 );
